4bd63937ac0f7d85bf02a9298d70cede41c9952e
gyordanoreis <gyordano@nexoos.com.br>
2019-04-09 13:46:28 -0300

    release 1.0.0

--- lib/clicksign/api/version.rb
+++ lib/clicksign/api/version.rb
@@ -1,5 +1,5 @@
 module Clicksign
   module Api
-    VERSION = "0.0.3"
+    VERSION = "1.0.0"
   end
 end
--- spec/clicksign/api_spec.rb
+++ spec/clicksign/api_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Clicksign::Api do
   it 'has a version number' do
-    expect(Clicksign::Api::VERSION).to eq('0.0.4')
+    expect(Clicksign::Api::VERSION).to eq('1.0.0')
   end
 
   describe '.configure' do
