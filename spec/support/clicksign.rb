d37609ab7e1ac04af556b8046c6199e7ef0a14cb
renan.proenca <renan.maringolo@gmail.com>
2021-10-29 17:03:35 -0300

    fix path file

--- spec/support/clicksign.rb
+++ spec/support/clicksign.rb
@@ -1,4 +1,4 @@
-file_path = File.expand_path('.././spec/fixtures/credentials.yml', __FILE__)
+file_path = File.expand_path('../../fixtures/credentials.yml', __FILE__)
 credentials = YAML.load(ERB.new(File.read(file_path)).result)
 
 Clicksign::API.configure do |config|
